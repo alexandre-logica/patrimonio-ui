39d4300edbebb585cb0d62549d8dfb8d1c9cb9a8
Alexandre Ferreira <alexandre.logica@gmail.com>
2018-02-23 00:18:35 -0300

    Projeto Angular consumindo serviço do Spring Boot

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import {TableModule} from 'primeng/table';
+import {ButtonModule} from 'primeng/button';
+import {InputTextModule} from 'primeng/inputtext';
+import {InputMaskModule} from 'primeng/inputmask';
+import {CalendarModule} from 'primeng/calendar';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ItemCadastroComponent } from './item-cadastro/item-cadastro.component';
+import { ItemService } from './item.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    ItemCadastroComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+
+    TableModule,
+    ButtonModule,
+    InputTextModule,
+    InputMaskModule,
+    CalendarModule
+  ],
+  providers: [
+    ItemService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
